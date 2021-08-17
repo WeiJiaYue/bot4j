d91398798ca4e8cfee67c5ad4bc82671b2c68944
lewisyu <lewis.yu@asiahr.com>
2021-08-17 17:28:52 +0800

    init

--- src/main/java/bot/MAProcessor.java
+++ src/main/java/bot/MAProcessor.java
@@ -2,10 +2,12 @@ package bot;
 
 import bot.excel.ExcelDatum;
 import bot.excel.ExcelProcessor;
+import org.ta4j.core.Bar;
 import org.ta4j.core.BarSeries;
 import org.ta4j.core.BaseBarSeriesBuilder;
 import org.ta4j.core.indicators.SMAIndicator;
 import org.ta4j.core.indicators.helpers.ClosePriceIndicator;
+import org.ta4j.core.num.Num;
 
 import java.math.BigDecimal;
 import java.time.ZoneId;
@@ -19,8 +21,8 @@ public class MAProcessor extends ExcelProcessor {
 
 
     public static BarSeries series = new BaseBarSeriesBuilder().build();
-    public static List<BigDecimal> sma5Results = new ArrayList<>();
-    public static List<BigDecimal> sma10Results = new ArrayList<>();
+    public static List<Double> sma5Results = new ArrayList<>();
+    public static List<Double> sma10Results = new ArrayList<>();
 
     public MAProcessor(String filepath, String filename) {
         super(filepath, filename);
@@ -30,6 +32,7 @@ public class MAProcessor extends ExcelProcessor {
     public void doProcess(ExcelDatum raw) throws Exception {
         raw.getHeaders().add("SMA5");
         raw.getHeaders().add("SMA10");
+        raw.getHeaders().add("Ops");
         List<Map<String, String>> rows = raw.getDatum();
         int index = 0;
         for (Map<String, String> row : rows) {
@@ -48,11 +51,40 @@ public class MAProcessor extends ExcelProcessor {
             ClosePriceIndicator closePrice = new ClosePriceIndicator(series);
             SMAIndicator sma5 = new SMAIndicator(closePrice, 5);
             SMAIndicator sma10 = new SMAIndicator(closePrice, 10);
+            Num sma5Value = sma5.getValue(index);
+            Num sma10Value = sma10.getValue(index);
+            row.put("SMA5", String.valueOf(sma5Value));
+            row.put("SMA10", String.valueOf(sma10Value));
+            sma5Results.add(sma5Value.doubleValue());
+            sma10Results.add(sma10Value.doubleValue());
+
+            if (index >= 10) {
+                double ma5 = sma5Value.doubleValue();
+                double ma10 = sma10Value.doubleValue();
 
 
+                if (ma10 > ma5) {
+                    System.out.println("==> 开多并且平空");
+                    int stopLossIdx = index;
+                    double stopLoss = series.getBar(stopLossIdx--).getLowPrice().doubleValue();
+                    for (int i = stopLossIdx; i > stopLossIdx - 5; i--) {
+                        double other = series.getBar(i).getLowPrice().doubleValue();
+                        stopLoss = Math.min(stopLoss, other);
+                    }
+                    Bar current = series.getBar(index);
 
-            row.put("SMA5",String.valueOf(sma5.getValue(index)));
-            row.put("SMA10",String.valueOf(sma10.getValue(index)));
+                    System.out.println("==> 开多点位：" + current.getOpenPrice().doubleValue() + " 止损点：" + stopLoss);
+
+
+                }
+                if (ma10 == ma5) {
+//                    System.out.println("待定");
+                }
+                if (ma10 < ma5) {
+//                    System.out.println("开空且平多");
+                }
+            }
+
             index++;
         }
     }
@@ -61,8 +93,28 @@ public class MAProcessor extends ExcelProcessor {
 
         //init series
         MAProcessor processor = new MAProcessor(ExcelProcessor.filePath, SnapshotGenerator.snapshot_file_name);
+        processor.setNeededGenerateNewExcel(false);
         processor.process();
 
+//        System.out.println(sma5Results);
+//        System.out.println(sma10Results);
+
+//
+//        for (int i = 10; i < sma10Results.size(); i++) {
+//            double ma5 = sma5Results.get(i);
+//            double ma10 = sma10Results.get(i);
+//
+//            if (ma10 > ma5) {
+//                System.out.println("多并且平空");
+//            }
+//            if (ma10 == ma5) {
+//                System.out.println("待定");
+//            }
+//            if (ma10 < ma5) {
+//                System.out.println("开空且平多");
+//            }
+//
+//        }
 
 
     }
